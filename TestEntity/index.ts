f92449a85e49b4eccc356ad0d9a90fdb2f83bac1
Konstantin Lepeshenkov <konstantin.lepeshenkov@gmail.com>
2021-04-02 17:54:55 +0200

    Update index.ts

--- TestEntity/index.ts
+++ TestEntity/index.ts
@@ -8,6 +8,7 @@ export default DurableFunctions.entity(async ctx => {
     // Signalling ourselves
     ctx.df.signalEntity(ctx.df.entityId, ctx.df.operationName);
 
+    // Emulating some activity. 3.5 sec was obtained experimentally, seems to maximize the chances
     await new Promise(resolve => setTimeout(resolve, 3500));
 
     console.log(`>>> Finished executing TestEntity.${ctx.df.operationName}`);
